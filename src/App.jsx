f12c01cd2e3ebf6e04dfba793f8bdf1cea0ea831
Aleksandr Omarov <Linuh.ru@yandex.ru>
2026-07-04 21:19:15 +0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,4 @@ function App() {
   );
 }
 
-function App() {
-  return (
-    <div>
-      <h1>Привет, мир!</h1>
-      <p>Это мой собственный проект на React.</p>
-    </div>
-  )
-}
-
 export default App;
