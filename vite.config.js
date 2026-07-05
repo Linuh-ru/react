2fd3b1ba84bcc17331f6a1952ab7b3604ccd5dc1
Aleksandr Omarov <Linuh.ru@yandex.ru>
2026-07-05 19:32:59 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // base: '/react/', // Замените на точное имя вашего репо на Git // СТАРОЕ ЗНАЧЕНИЕ НЕАКТУАЛЬНОЕ! нужно при первночальной настройке
-  base: '/', // Сюда ставим просто слэш!
+  base: './', // Сюда ставим просто слэш!
 })
